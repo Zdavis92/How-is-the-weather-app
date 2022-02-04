07ed572e148e3108c3122d2591accaf83f929015
Zach Davis <zacharydavis92@gmail.com>
2022-02-03 20:36:49 -0600

    added current date and stored weather info the the next 5 days

--- assets/script.js
+++ assets/script.js
@@ -1,12 +1,50 @@
 var lat = ""
 var lon = ""
 var cityName = ""
+var date = moment().format("MM DD YYYY");
 var cityNameEl = document.querySelector("#searchBox")
 var buttonEl = document.querySelector("#searchBtn");
+var currentCon = {
+    temp: "",
+    wind: "",
+    humidity: "",
+    UVIndex: ""
+}
+var daysArr = [
+    day1 = {
+    date: "",
+    temp: "",
+    wind: "",
+    humidity: ""
+},
+    day2 = {
+    date: "",
+    temp: "",
+    wind: "",
+    humidity: ""
+},
+    day3 = {
+    date: "",
+    temp: "",
+    wind: "",
+    humidity: ""
+},
+    day4 = {
+    date: "",
+    temp: "",
+    wind: "",
+    humidity: ""
+},
+    day5 = {
+    date: "",
+    temp: "",
+    wind: "",
+    humidity: ""
+}]
 
 var search = function(event) {
     cityName = cityNameEl.value
-    console.log(cityName)
+    cityNameEl.value = ""
     getCityLatLon(cityName);
 }
 
@@ -15,11 +53,8 @@ var getCityLatLon = function(cityName) {
         var apiCity = "http://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=1&appid=7c0bd0cf3800dbf86808087317e3514f"
         fetch(apiCity).then(function(response) {
             response.json().then(function(data){
-                console.log(data);
                 lat = data[0].lat
                 lon = data[0].lon
-                console.log(lat);
-                console.log(lon);
                 getWeatherData(lat, lon);
             })
         })
@@ -34,7 +69,23 @@ var getWeatherData = function(lat, lon) {
     fetch(apiLatLon).then(function(response) {
         response.json().then(function(data) {
             console.log(data)
+            currentCon.temp = data.current.temp
+            currentCon.wind = data.current.wind_speed
+            currentCon.humidity = data.current.humidity
+            currentCon.UVIndex = data.current.uvi
+            for (i = 0; i < daysArr.length; i++) {
+                daysArr[i].date = moment().add(i+1, 'days').format("MM DD YYYY")
+                daysArr[i].temp = data.daily[i].temp.day
+                daysArr[i].wind = data.daily[i].wind_speed
+                daysArr[i].humidity = data.daily[i].humidity
+            }
+            console.log(daysArr);
         })
     })
 }
+
+var displyWeather = function() {
+
+}
 buttonEl.addEventListener("click", search)
+console.log(date);
